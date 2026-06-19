6e5dafa214575f8045a979552b5d70283ca89c68
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-20 01:00:08 +0200

    add cachy kernel and aagl

--- modules/hosts/desktop/default.nix
+++ modules/hosts/desktop/default.nix
@@ -1,8 +1,23 @@
 { self, inputs, ... }: {
     flake.nixosConfigurations.mustelid = inputs.nixpkgs.lib.nixosSystem {
         modules = [
+
+        ({ ... }: {
+          imports = [ inputs.aagl.nixosModules.default ];
+          nix.settings = inputs.aagl.nixConfig;
+          programs.anime-game-launcher.enable = true;
+        })
+
+
+        # cachy kernel
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
+          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-x86_64-v3;
+        })
+
         self.nixosModules.mustelidConfiguration
         self.nixosModules.home-manager
         ];
+        specialArgs = { inherit inputs; };
       };
   }
