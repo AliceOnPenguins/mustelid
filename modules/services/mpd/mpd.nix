69a8065f64432739bef273fd198ebe48f2ec55b8
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-20 00:35:22 +0200

    attempt at fixing build error

--- modules/services/mpd/mpd.nix
+++ modules/services/mpd/mpd.nix
@@ -1,10 +1,10 @@
-{ self, inputs, ... }: {
-    flake.homeModules.mpd = { pkgs, lib, ... }: {
+{ self, inputs, config, ... }: {
+    flake.homeModules.mpd = { pkgs, lib, config, ... }: {
         services.mpd = {
             enable = true;
             network.listenAddress = "/run/user/1000/mpd/socket";
-            musicDirectory = "~/Music";
-            dataDir = "~/.local/share/mpd";
+            musicDirectory = "${config.home.homeDirectory}/Music";
+            dataDir = "${config.home.homeDirectory}/.local/share/mpd";
             extraConfig = ''
               audio_output {
                   type "pipewire"
