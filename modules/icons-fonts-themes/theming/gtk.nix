14dff1659d9b941c6d1ad426719a90dda0a21885
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-22 14:22:00 +0200

    add gtk theming

---
+++ modules/icons-fonts-themes/theming/gtk.nix
@@ -0,0 +1,51 @@
+{ self, inputs, ... }: {
+  flake.homeModules.gtk = { pkgs, lib, ... }: {
+    gtk = {
+      enable = true;
+      theme = {
+        name = "adw-gtk3";
+        package = pkgs.adw-gtk3;
+      };
+      iconTheme = {
+        name = "Papirus";
+        package = pkgs.papirus-icon-theme;
+      };
+      font = {
+        name = "JetBrainsMono Nerd Font";
+        size = 14;
+      };
+      cursorTheme = {
+        name = "Remus-White";
+        size = 32;
+      };
+      gtk2.extraConfig = ''
+        gtk-toolbar-style=GTK_TOOLBAR_ICONS
+        gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+        gtk-button-images=0
+        gtk-menu-images=0
+        gtk-enable-event-sounds=1
+        gtk-enable-input-feedback-sounds=0
+        gtk-xft-antialias=1
+        gtk-xft-hinting=1
+        gtk-xft-hintstyle="hintslight"
+        gtk-xft-rgba="rgb"
+      '';
+      gtk3.extraConfig = {
+        gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
+        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+        gtk-button-images = 0;
+        gtk-menu-images = 0;
+        gtk-enable-event-sounds = 1;
+        gtk-enable-input-feedback-sounds = 0;
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintslight";
+        gtk-xft-rgba = "rgb";
+        gtk-application-prefer-dark-theme = 1;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+  };
+}
