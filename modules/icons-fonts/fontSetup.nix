349fe398751763cd1f84c5386d946a0b48117204
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-22 13:09:08 +0200

    add fallback fonts + clean up font module

--- modules/icons-fonts/fontSetup.nix
+++ modules/icons-fonts/fontSetup.nix
@@ -1,8 +1,11 @@
 { self, inputs, ... }: {
     flake.nixosModules.fontSetup = { pkgs, lib, ... }: {
         fonts = {
-            packages = [
-              pkgs.nerd-fonts.jetbrains-mono
+            packages = with pkgs; [
+              nerd-fonts.jetbrains-mono
+              noto-fonts
+              noto-fonts-cjk-sans
+              noto-fonts-emoji
             ];
           };
       };
