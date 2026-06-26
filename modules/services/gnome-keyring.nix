69e0ee6bdf869dd2c2ef28485ba0d2abb36fdbe7
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-06-26 10:30:56 +0200

    fix pam

--- modules/services/gnome-keyring.nix
+++ modules/services/gnome-keyring.nix
@@ -1,5 +1,6 @@
 { self, inputs, ... }: {
     flake.nixosModules.gnome-keyring = { pkgs, lib, ... }: {
+        security.pam.services.login.enableGnomeKeyring = true;
         services.gnome.gnome-keyring = {
             enable = true;
           };
